5c3109a2b4878a00218e2ef128ddd1409ef6a3bd
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2024-06-25 23:07:29 +0600

    Wrapping Everything Router Agent

--- agent.py
+++ agent.py
@@ -32,16 +32,42 @@ def main():
     # )
 
 
-    csv_agent = create_csv_agent(
-        ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-        path="./episode-info.csv",
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
-        allow_dangerous_code=True
-    )
-    csv_agent.run("How many columns are there in file csv file")
-    csv_agent.run("which writer wrote the most episode? How many episodes did he write?")
+    # csv_agent = create_csv_agent(
+    #     ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+    #     path="./episode-info.csv",
+    #     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    #     verbose=True,
+    #     allow_dangerous_code=True
+    # )
+    # csv_agent.run("How many columns are there in file csv file")
+    # csv_agent.run("which writer wrote the most episode? How many episodes did he write?")
 
+    grand_agent = initialize_agent(tools=[
+        Tool(
+            name="PythonAgent",
+            func=python_agent_executor.invoke,
+            description= """useful when you need to transform natural language and write from it python and execute the python code, returning the results of the code execution, DO NOT SEND PYTHON CODE TO THIS TOOL"""
+        ),
+        Tool(
+            name="CSVAgent",
+            func=csv_agent.run,
+            description= """useful when you need to answer question over episode_info.csv file, takes an input the entire question and returns the answer after running pandas calculations"""
+        )
+    ],
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo"),
+    agent_type= AgentType.OPENAI_FUNCTIONS,
+    verbose=True,)
+
+    grand_agent.run(
+        """generate and save in current working directory 2 QRcodes that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    )
+
+    
+# retriever_tool = create_retriever_tool(
+#     retriever,
+#     "langsmith_search",
+#     "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
+# )
 
 if __name__ == "__main__":
     main()
